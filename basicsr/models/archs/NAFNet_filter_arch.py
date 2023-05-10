7f1a39bdc55212fec89cf5239c4f23d333b8e0ba
dhryou <dhryou@snu.ac.kr>
2023-05-10 19:06:26 +0900

    ff

--- basicsr/models/archs/NAFNet_filter_arch.py
+++ basicsr/models/archs/NAFNet_filter_arch.py
@@ -50,6 +50,7 @@ class Adaptive_freqfilter_regression(nn.Module):
         # self.radius_factor_set = torch.tensor([0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.85, 1.0]).cuda()
         # self.radius_factor_set = torch.tensor([0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8 , 0.85, 0.9, 0.95, 1.0]).cuda()
         
+        
   
         self.fclayer_v1 = nn.Linear(256, 512)
         self.fclayer_v2 = nn.Linear(512, len(self.radius_factor_set))
